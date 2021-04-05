7db847d93537014ad95a175c0abf169459f68fef
edwinaringo <tedaringo628@gmail.com>
2021-04-06 00:42:27 +0300

    adding routing

--- gitSearch/src/app/app-routing.module.ts
+++ gitSearch/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent},
+  { path: 'repositories', component: RepositoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
