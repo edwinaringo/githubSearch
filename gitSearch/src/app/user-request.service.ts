657118fa4885f19fe6bb5f7a9766f7b24c1a92e6
edwinaringo <tedaringo628@gmail.com>
2021-04-05 23:33:26 +0300

    Adding promises

--- gitSearch/src/app/user-request.service.ts
+++ gitSearch/src/app/user-request.service.ts
@@ -7,7 +7,34 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UserRequestService {
-  user!: User;
+  user: User;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.user = new User("","","","");
+  }
+
+  searchUsersRequest(searchName: string){
+    interface ApiResponse{
+      login:string;
+      avatar_url:string;
+      followers_url: string;
+      following_url: string;
+    }
+    let promise = new Promise<void>((resolve,reject)=>{
+      this.http.get<ApiResponse>('https://api.github.com/search/users/'+searchName+'?access_token='+environment.apiKey).toPromise().then(response=>{
+        this.user.login = response.login
+        this.user.avatar_url = response.avatar_url
+        this.user.followers_url = response.followers_url
+        this.user.following_url = response.following_url
+
+        resolve()
+      },
+      error=>{
+        this.user.login = "You got an error"
+
+        reject(error)
+      })
+    })
+    return promise
+  }
 }
