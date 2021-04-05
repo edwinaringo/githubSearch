786a31190a3377c4017cf6e6de633a16adc56bac
edwinaringo <tedaringo628@gmail.com>
2021-04-06 00:25:58 +0300

    Adding logic to the service

--- gitSearch/src/app/repo-request.service.ts
+++ gitSearch/src/app/repo-request.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoRequestService {
+  foundRepo!: Repository[];
 
-  constructor() { }
+  constructor(private https: HttpClient) { 
+    this.foundRepo;
+  }
+
+  searchReposRequest(searchName: string){
+    interface ApiResponse{
+      total_count:number;
+      incomplete_results:boolean;
+      items: Repository[];
+    }
+    let options = {
+      headers: {
+        'Authorization': 'Basic ' + btoa(environment.apiKey)
+      },
+      params: {
+        'q': searchName,
+      }
+    }
+    let promise = new Promise<void>((resolve,reject)=>{
+      this.https.get<ApiResponse>('https://api.github.com/search/repositories', options).toPromise().then(response=>{
+        this.foundRepo = response.items
+        resolve()
+      },
+      error=>{
+        this.foundRepo = []
+
+        reject(error)
+      })
+    })
+    return promise
+  }
 }
