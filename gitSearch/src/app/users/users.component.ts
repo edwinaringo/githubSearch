dd1dc6ec9c36b7620d491b3e7cbaef264292438d
edwinaringo <tedaringo628@gmail.com>
2021-04-07 15:43:52 +0300

    Adding styling

--- gitSearch/src/app/users/users.component.ts
+++ gitSearch/src/app/users/users.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Repository } from '../repository';
 import { Search } from '../Search';
 import { User } from '../user';
 import { UserRequestService } from '../user-request.service';
+import { RepoRequestService } from '../repo-request.service';
 
 
 @Component({
@@ -11,10 +13,14 @@ import { UserRequestService } from '../user-request.service';
 })
 export class UsersComponent implements OnInit {
   users!: User[];
+  repos: Repository[] = [];
+  repo: any;
   searchName = "edwinaringo";
   searchInput = new Search('edwinaringo')
 
-  constructor(private userService:UserRequestService) { }
+  constructor(private userService:UserRequestService,private repoService:RepoRequestService ) { }
+  
+
   mySearch(searchName: string) {
     this.userService.searchUsersRequest(searchName).then(
       (success)=>{
@@ -24,7 +30,14 @@ export class UsersComponent implements OnInit {
         console.log(error)
       }
     );
-
+    this.repoService.searchRepoByUsernameRequest(searchName).then(
+      (success)=>{
+        this.repos = this.repoService.foundRepo;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    );
   }
 
 
